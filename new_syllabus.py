a10aab62e95b08bb60dca7b89ba644d80848a8fb
Nick D <ndanis@gmail.com>
2021-06-04 12:04:43 -0500

    new syllabi from template

--- new_syllabus.py
+++ new_syllabus.py
@@ -12,18 +12,30 @@ def build_header(course, semester):
     header = f"---\nlayout: syllabus\n"
     header += f"title: {course}\n"
     header += f"semester: {semester}\n"
+    header += f"time: \n"
+    header += f"location: \n"
+    header += f"canvas-url: \n"
+    header += f"published: false\n"
     header += f"---\n\n"
-    header += "* TOC\n{:toc}\n"
-    header += "\n TKTKTK"
     return header
 
 semester, file_semester = get_semester()
 course, file_course = get_course()
 
 filename = f"{file_semester}-{file_course}.md"
-contents = build_header(course, semester)
+header = build_header(course, semester)
 
-with open(f"_syllabi/{filename}", "w") as f:
-    f.write(contents)
+
+
+skeleton = []
+with open("_syllabi/skeleton.txt","r",encoding="utf-8") as s:
+    for line in s:
+        skeleton.append(line)
+
+with open(f"_syllabi/{filename}", "w",encoding="utf-8") as f:
+    f.write(header)
+    for line in skeleton:
+        f.write(line)
+
 
 print(f"New syllabus created: {filename}")
