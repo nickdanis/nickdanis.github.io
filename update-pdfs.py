5d264f24521f584e9d6f6cb059a9958a6b28e85e
Nick D <ndanis@gmail.com>
2021-06-17 09:56:49 -0500

    updated script and links

--- update-pdfs.py
+++ update-pdfs.py
@@ -1,5 +1,8 @@
 import yaml, os, re
 from pyhtml2pdf import converter
+from dateutil.parser import parse
+
+update_after = parse('June 10, 2021')
 
 def get_tags(raw_contents):
     y_start = raw_contents.index('---\n')
@@ -18,12 +21,21 @@ for syl in syllabi:
             raw_contents.append(line)
     tags = get_tags(raw_contents)
     if tags.get('pdf') == True:
-        urlname = re.sub(r'\.md','',syl)
-        pdfname = re.sub(r'\.md','.pdf',syl)
-        converter.convert(
-            f'https://www.nickdanis.com/syllabi/{urlname}', 
-            f'assets/pdfsyllabi/{pdfname}')
+        last_edited = parse(tags.get('last-updated'))
+        if update_after < last_edited:
+            urlname = re.sub(r'\.md','',syl)
+            pdfname = re.sub(r'\.md','.pdf',syl)
+            converter.convert(
+                f'https://www.nickdanis.com/syllabi/{urlname}', 
+                f'assets/pdfsyllabi/{pdfname}')
 
 
 # convert cv
-converter.convert(f'https://www.nickdanis.com/cv/', f'assets/pdfs/danis-cv.pdf') 
+cv_contents = []
+with open('cv.md','r') as file:
+    for line in file:
+        cv_contents.append(line)
+    tags = get_tags(cv_contents)
+    last_edited = parse(tags.get('last-updated'))
+    if update_after < last_edited:
+        converter.convert(f'https://www.nickdanis.com/cv/', f'assets/pdfs/danis-cv.pdf') 
